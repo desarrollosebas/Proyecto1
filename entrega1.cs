75c79868405ea2dfa87a89aecffb0a2dda34ea3e
soy el sebas <desarrollosebas177@gmail.com>
2026-02-26 17:41:56 -0500

    variables y entrada de datos para el programa de pedidos

--- entrega1.cs
+++ entrega1.cs
@@ -1,1 +1,10 @@
-Console.WriteLine("hvghvjh");
+int montopedido = 0;
+string ciudadpedido = "";
+bool tipodecliente;
+int cantidadpedido = 0;
+
+Console.WriteLine("Ingrese el monto del pedido:");
+
+Console.WriteLine("Ingrese la ciudad del pedido:");
+
+Console.WriteLine("Ingrese el tipo de cliente (true para mayorista, false para minorista):");
