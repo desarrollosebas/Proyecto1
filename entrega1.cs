c6f379f3aff291e2848a543d6c1739e6c43284c4
desarrollosebas <desarrollosebas117@gmail.com>
2026-05-16 23:08:45 -0500

    Actualizar la visualización del menú y los métodos de gestión de opciones
    Reestructurar la visualización del menú y la gestión de opciones en el sistema de entrega de paquetes.

--- entrega1.cs
+++ entrega1.cs
@@ -1,42 +1,16 @@
-using System;
-using System.Collections.Generic;
-
-class enviodepaquetes
-{
-    static void Main()
+  /// <summary>
+    /// Muestra las opciones disponibles del sistema.
+    /// </summary>
+    static void MostrarMenu()
     {
-        // Listas donde se guardará el historial y los costos de cada envío
-        List<string> historialEnvios = new List<string>();
-        List<decimal> costosEnvios = new List<decimal>();
-
-        // Main solo se encarga de iniciar el programa
-        EjecutarMenu(historialEnvios, costosEnvios);
+        Console.WriteLine("\n=== MENÚ DE ENVÍOS ===");
+        Console.WriteLine("1. Calcular envío");
+        Console.WriteLine("2. Ver historial");
+        Console.WriteLine("3. Salir");
+        Console.Write("Seleccione una opción: ");
     }
 
     /// <summary>
-    /// Controla el flujo principal del programa y las opciones del menú.
+    /// Lee y valida la opción ingresada por el usuario.
     /// </summary>
-    /// <param name="historialEnvios">Lista donde se guardan los resúmenes de los envíos.</param>
-    /// <param name="costosEnvios">Lista donde se guardan los costos finales.</param>
-    static void EjecutarMenu(List<string> historialEnvios, List<decimal> costosEnvios)
-    {
-        int opcionMenu;
-
-        do
-        {
-            MostrarMenu();
-            opcionMenu = LeerOpcionMenu();
-
-            // Si el usuario quiere calcular un envío
-            if (opcionMenu == 1)
-            {
-                ProcesarEnvio(historialEnvios, costosEnvios);
-            }
-            // Si el usuario quiere ver el historial
-            else if (opcionMenu == 2)
-            {
-                MostrarHistorial(historialEnvios, costosEnvios);
-            }
-
-        } while (opcionMenu != 3);
-    }
+    /// <returns>Retorna una opción válida.</returns>
